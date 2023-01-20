f65356823ad94fc5ad48e2b5dc17831bc257000f
Chiranjib Dash <work.chiranjib@gmail.com>
2023-01-20 18:07:33 +0530

    Added Dao interfaces,one method and Model for Note data

---
+++ src/main/java/com/dao/DBOperation.java
@@ -0,0 +1,8 @@
+package com.dao;
+
+public interface DBOperation {
+
+	public boolean findData();
+	public boolean saveData();
+
+}
---
+++ src/main/java/com/dao/NoteOperation.java
@@ -0,0 +1,17 @@
+package com.dao;
+
+public class NoteOperation implements DBOperation {
+
+	@Override
+	public boolean findData() {
+		
+		return false;
+	}
+
+	@Override
+	public boolean saveData() {
+		
+		return false;
+	}
+
+}
