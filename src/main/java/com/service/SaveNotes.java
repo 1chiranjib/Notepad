11496c108eb7d42d53584dac4096f9d936fbd034
Chiranjib Dash <work.chiranjib@gmail.com>
2023-01-21 01:36:54 +0530

    Added multiple pages and completed insert operation flow

---
+++ src/main/java/com/service/SaveNotes.java
@@ -0,0 +1,35 @@
+package com.service;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.business.RequestProcessor;
+
+@WebServlet("/savenote")
+public class SaveNotes extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    public SaveNotes() {
+        super();
+    }
+
+    @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String afterCommit = RequestProcessor.getInstance().processRequest(request);
+		
+		try {
+			PrintWriter writer = response.getWriter();
+			writer.println("<h2 style='text-align:center;'>"+afterCommit+"</h2>");
+			writer.println("<h2 style='text-align:center;'><a href='allnotes.jsp'>Click here to see all notes</h2>");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
